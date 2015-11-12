d3cf4cda42c177f4e4dffc7ab1a4514c48406f64
allanfish <googleyufei@qq.com>
2015-11-12 20:28:35 +0800

    update files

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
-module.exports = function(ret, conf, settings, opt) {
-    var tpl = "<li><a href='{href}'>{title}</a></li>";
+var path = require('path'),
+    listFilePath = path.join(__dirname, "./list.html"),
+    content = fis.util.read(listFilePath);
+
+module.exports = function (ret, conf, settings, opt) {
     var folders = {};
-    fis.util.map(ret.src, function(subpath, file) {
+    fis.util.map(ret.src, function (subpath, file) {
         if (file.isHtmlLike) {
-            if (!folders[file.subdirname]) {
-                folders[file.subdirname] = {
+            var folderObj = folders[file.subdirname];
+            if (!folderObj) {
+                folderObj = folders[file.subdirname] = {
                     files: [],
                     listFile: null
                 }
             }
-
-            var folderObj = folders[file.subdirname];
-            if (file.basename === 'list.html') {
+            if (!!folderObj.listFile) {
+                return false;
+            } else if (file.basename === 'list.html') {
                 folderObj.listFile = file;
             } else {
                 folderObj.files.push(file);
+                folderObj.dirname = file.dirname;
             }
         }
     });
-    fis.util.map(folders, function(_subdirname, folderObj) {
-        var listFile = folderObj.listFile;
-        if (!folderObj.listFile) {
-            return false;
-        }
-        var body = [];
-        folderObj.files.forEach(function(file) {
-            var matches = file.getContent().match(/\<title\>\S*\-(\S*)<\/title>/i);
-            if (!matches) {
-                matches = file.getContent().match(/\<title\>(\S*)<\/title>/i)
+    fis.util.map(folders, function (_subdirname, folderObj) {
+        if (folderObj.files.length > 1) {
+            var listFile = folderObj.listFile;
+            if (!listFile) {
+                fis.file.wrap(folderObj.dirname + "/list.html");
+                listFile.setContent(content);
             }
-            var title = matches ? RegExp.$1 : file.basename;
-            body.push(tpl.replace('{href}', file.basename).replace('{title}', title));
-        });
-        listFile.setContent(listFile.getContent().replace('${list}', body.join("\r\n")));
-        fis.log.debug("generate list.html ok");
+            generateListFile(listFile, folderObj.files);
+            fis.log.debug("generate list.html ok");
+        }
     });
+}
+
+function generateListFile(listFile, files) {
+    var body = [],
+        tpl = "<li><a href='{href}'>{title}</a></li>";
+
+    files.forEach(function (file) {
+        var matches = file.getContent().match(/\<title\>\S*\-(\S*)<\/title>/i);
+        if (!matches) {
+            matches = file.getContent().match(/\<title\>(\S*)<\/title>/i)
+        }
+        var title = matches ? RegExp.$1 : file.basename;
+        body.push(tpl.replace('{href}', file.basename).replace('{title}', title));
+    });
+    listFile.setContent(listFile.getContent().replace('${list}', body.join("\r\n")));
 }
